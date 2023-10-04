cf1dede6dc508484ffdcbd5ea8efc47a49decf35
NextRey <hamaq134@gmail.com>
2023-10-04 22:00:20 +0300

    Рабочая версия начало разработки

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,14 +1,16 @@
 package com.example.examplemod;
 
 import net.minecraft.init.Blocks;
+import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.Event;
 
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "helpertest";
     public static final String VERSION = "1.0";
     
     @EventHandler
@@ -16,5 +18,6 @@ public class ExampleMod
     {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+        System.out.println(Blocks.grass.getMaterial());
     }
 }
